273d2e0bb6393c570c731684fe22bc5bb4dde30e
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-17 13:27:12 +0100

    [DONE] adjust spacing

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ let km = parseInt(prompt('Inserisci quanti km vuoi percorrere'));
 let priceKm = 0.21;
 let ticketPrice
 
-if(age<18){
+if(age < 18){
     console.log('Verra\' applicato lo sconto per minorenni');
     ticketPrice = (km * priceKm) * 0.8;
     console.log('Il prezzo del biglietto e\' ' + ticketPrice.toFixed(2) + "€")
 }
-else if(age<=65){
+else if(age <= 65){
     console.log('Non verra\' applicato alcuno sconto');
     ticketPrice = (km * priceKm);
     console.log('Il prezzo del biglietto e\' ' + ticketPrice.toFixed(2) + "€");
