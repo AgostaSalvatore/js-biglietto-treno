a4d6e6c8ce1676a3bcfc734408f32a976cdd295b
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-17 13:25:33 +0100

    [DONE] format ticket price output to two decimal places

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ let ticketPrice
 if(age<18){
     console.log('Verra\' applicato lo sconto per minorenni');
     ticketPrice = (km * priceKm) * 0.8;
-    console.log('Il prezzo del biglietto e\' ' + ticketPrice + "€")
+    console.log('Il prezzo del biglietto e\' ' + ticketPrice.toFixed(2) + "€")
 }
 else if(age<=65){
     console.log('Non verra\' applicato alcuno sconto');
     ticketPrice = (km * priceKm);
-    console.log('Il prezzo del biglietto e\' ' + ticketPrice + "€");
+    console.log('Il prezzo del biglietto e\' ' + ticketPrice.toFixed(2) + "€");
 }
 else{
     console.log('Verra\' applicato lo sconto per over 65');
     ticketPrice = (km * priceKm) * 0.6;
-    console.log('Il prezzo del biglietto e\' ' + ticketPrice + "€")
+    console.log('Il prezzo del biglietto e\' ' + ticketPrice.toFixed(2) + "€")
 }
