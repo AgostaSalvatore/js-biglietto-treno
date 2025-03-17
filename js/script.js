88a2a3343a65da14c4f3bc60f504ebe3dabf37aa
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-17 13:20:22 +0100

    [feat] ticket pricing logic based on age (minor)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 let age = parseInt(prompt('Inserisci la tua eta\' '));
 let km = parseInt(prompt('Inserisci quanti km vuoi percorrere'));
 let priceKm = 0.21;
+let ticketPrice
+
+if(age<18){
+    console.log('Verra\' applicato lo sconto per minorenni');
+    ticketPrice = (km * priceKm) * 0.8;
+    console.log('Il prezzo del biglietto e\' ' + ticketPrice + "€")
+}
