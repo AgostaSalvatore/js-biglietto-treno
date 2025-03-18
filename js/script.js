30645e58a78183445b5081dafaf01c2eab63e3f1
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-18 09:41:10 +0100

    feat: simplify ticket price calculation and apply discounts directly to price per kilometer

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@ let age = parseInt(prompt('Inserisci la tua eta\' '));
 let km = parseInt(prompt('Inserisci quanti km vuoi percorrere'));
 
 // indicare il prezzo al chilometro
-let priceKm = 0.21;
-let ticketPrice
+let priceKm = km * 0.21;
+
+console.log(`i dati sono: ${age} anni e ${km} km`)
 
 if(age < 18){ //sconto per i minorenni
     console.log('Verra\' applicato lo sconto per minorenni');
-    ticketPrice = (km * priceKm) * 0.8;
-    console.log('Il prezzo del biglietto e\' ' + ticketPrice.toFixed(2) + "€")
+    priceKm= priceKm * 0.8;
+    console.log('Il prezzo del biglietto e\' ' + priceKm.toFixed(2) + "€")
 }
 else if(age <= 65){ //nessuno sconto per eta' compresa tra 18 64
     console.log('Non verra\' applicato alcuno sconto');
-    ticketPrice = (km * priceKm);
-    console.log('Il prezzo del biglietto e\' ' + ticketPrice.toFixed(2) + "€");
+    console.log('Il prezzo del biglietto e\' ' + priceKm.toFixed(2) + "€");
 }
 else{ //sconto per gli over 65
     console.log('Verra\' applicato lo sconto per over 65');
-    ticketPrice = (km * priceKm) * 0.6;
-    console.log('Il prezzo del biglietto e\' ' + ticketPrice.toFixed(2) + "€")
+    priceKm = priceKm * 0.6;
+    console.log('Il prezzo del biglietto e\' ' + priceKm.toFixed(2) + "€")
 }
