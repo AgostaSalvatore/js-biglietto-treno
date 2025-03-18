a830e3144ad03a2f83c522aa5b2a33b062bd48a8
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-18 09:46:15 +0100

    feat: improve ticket price output formatting and enhance discount logic for minors and seniors

--- js/script.js
+++ js/script.js
@@ -12,14 +12,16 @@ console.log(`i dati sono: ${age} anni e ${km} km`)
 if(age < 18){ //sconto per i minorenni
     console.log('Verra\' applicato lo sconto per minorenni');
     priceKm= priceKm * 0.8;
-    console.log('Il prezzo del biglietto e\' ' + priceKm.toFixed(2) + "€")
+    //proceKm *= 0.8 forma contratta
+    console.log(`Il prezzo del biglietto e' ${priceKm.toFixed(2)} €`)
 }
 else if(age <= 65){ //nessuno sconto per eta' compresa tra 18 64
     console.log('Non verra\' applicato alcuno sconto');
-    console.log('Il prezzo del biglietto e\' ' + priceKm.toFixed(2) + "€");
+    console.log(`Il prezzo del biglietto e' ${priceKm.toFixed(2)} €`);
 }
 else{ //sconto per gli over 65
     console.log('Verra\' applicato lo sconto per over 65');
     priceKm = priceKm * 0.6;
-    console.log('Il prezzo del biglietto e\' ' + priceKm.toFixed(2) + "€")
+    //priceKm *= 0.6
+    console.log(`Il prezzo del biglietto e' ${priceKm.toFixed(2)} €`)
 }
